814b26249dbf0e1106b2262888d6134dcc4a8204
StuartXuJian <StuartXuJian@gmail.com>
2023-05-08 10:28:52 +0800

    add one label

--- main.py
+++ main.py
@@ -55,70 +55,88 @@ def main():
     with gr.Blocks(title="ChatGPT工作利器", theme=set_theme, analytics_enabled=False, css=advanced_css) as demo:
         gr.HTML(title_html)
         cookies = gr.State({'api_key': API_KEY, 'llm_model': LLM_MODEL})
-        with gr_L1():
-            with gr_L2(scale=2):
-                chatbot = gr.Chatbot(label=f"当前模型：{LLM_MODEL}")
-                chatbot.style(height=CHATBOT_HEIGHT)
-                history = gr.State([])
-            with gr_L2(scale=1):
-                with gr.Accordion("输入区", open=True) as area_input_primary:
-                    with gr.Row():
-                        gr.Markdown(r"[User Guide](https://confluence.ext.net.nokia.com/display/RCP/Working+tool+user+guide)")
-                    with gr.Row():
-                        txt = gr.Textbox(show_label=False, placeholder="Input question here.").style(container=False)
-                    with gr.Row():
-                        submitBtn = gr.Button("提交", variant="primary")
-                    with gr.Row():
-                        resetBtn = gr.Button("重置", variant="secondary"); resetBtn.style(size="sm")
-                        stopBtn = gr.Button("停止", variant="secondary"); stopBtn.style(size="sm")
-                        clearBtn = gr.Button("清除", variant="secondary", visible=False); clearBtn.style(size="sm")
-                    with gr.Row():
-                        status = gr.Markdown(f"Tip: 按Enter提交, 按Shift+Enter换行。当前模型: {LLM_MODEL} \n {proxy_info}")
-                with gr.Accordion("基础功能区", open=True) as area_basic_fn:
-                    with gr.Row():
-                        for k in functional:
-                            variant = functional[k]["Color"] if "Color" in functional[k] else "secondary"
-                            functional[k]["Button"] = gr.Button(k, variant=variant)
-                with gr.Accordion("函数插件区", open=False) as area_crazy_fn:
-                    with gr.Row():
-                        gr.Markdown("注意：以下“红颜色”标识的函数插件需从输入区读取路径作为参数.")
-                    with gr.Row():
-                        for k in crazy_fns:
-                            if not crazy_fns[k].get("AsButton", True): continue
-                            variant = crazy_fns[k]["Color"] if "Color" in crazy_fns[k] else "secondary"
-                            crazy_fns[k]["Button"] = gr.Button(k, variant=variant)
-                            crazy_fns[k]["Button"].style(size="sm")
-                    with gr.Row():
-                        with gr.Accordion("更多函数插件", open=True):
-                            dropdown_fn_list = [k for k in crazy_fns.keys() if not crazy_fns[k].get("AsButton", True)]
-                            with gr.Row():
-                                dropdown = gr.Dropdown(dropdown_fn_list, value=r"打开插件列表", label="").style(container=False)
-                            with gr.Row():
-                                plugin_advanced_arg = gr.Textbox(show_label=True, label="高级参数输入区", visible=False, 
-                                                                 placeholder="这里是特殊函数插件的高级参数输入区").style(container=False)
-                            with gr.Row():
-                                switchy_bt = gr.Button(r"请先从插件列表中选择", variant="secondary")
-                    with gr.Row():
-                        with gr.Accordion("点击展开“文件上传区”。上传本地文件可供红色函数插件调用。", open=False) as area_file_up:
-                            file_upload = gr.Files(label="任何文件, 但推荐上传压缩文件(zip, tar)", file_count="multiple")
-                with gr.Accordion("更换模型 & SysPrompt & 交互界面布局", open=(LAYOUT == "TOP-DOWN")):
-                    system_prompt = gr.Textbox(show_label=True, placeholder=f"System Prompt", label="System prompt", value=initial_prompt)
-                    top_p = gr.Slider(minimum=-0, maximum=1.0, value=1.0, step=0.01,interactive=True, label="Top-p (nucleus sampling)",)
-                    temperature = gr.Slider(minimum=-0, maximum=2.0, value=1.0, step=0.01, interactive=True, label="Temperature",)
-                    max_length_sl = gr.Slider(minimum=256, maximum=4096, value=512, step=1, interactive=True, label="Local LLM MaxLength",)
-                    checkboxes = gr.CheckboxGroup(["基础功能区", "函数插件区", "底部输入区", "输入清除键", "插件参数区"], value=["基础功能区", "函数插件区"], label="显示/隐藏功能区")
-                    md_dropdown = gr.Dropdown(AVAIL_LLM_MODELS, value=LLM_MODEL, label="更换LLM模型/请求源").style(container=False)
+        with gr.Tab("基础功能区"):
+            with gr_L1():
+                with gr_L2(scale=2):
+                    chatbot = gr.Chatbot(label=f"当前模型：{LLM_MODEL}")
+                    chatbot.style(height=CHATBOT_HEIGHT)
+                    history = gr.State([])
+                with gr_L2(scale=1):
+                    with gr.Accordion("输入区", open=True) as area_input_primary:
+                        with gr.Row():
+                            gr.Markdown(r"[User Guide](https://confluence.ext.net.nokia.com/display/RCP/Working+tool+user+guide)")
+                        with gr.Row():
+                            txt = gr.Textbox(show_label=False, placeholder="Input question here.").style(container=False)
+                        with gr.Row():
+                            submitBtn = gr.Button("提交", variant="primary")
+                        with gr.Row():
+                            resetBtn = gr.Button("重置", variant="secondary"); resetBtn.style(size="sm")
+                            stopBtn = gr.Button("停止", variant="secondary"); stopBtn.style(size="sm")
+                            clearBtn = gr.Button("清除", variant="secondary", visible=False); clearBtn.style(size="sm")
+                        with gr.Row():
+                            status = gr.Markdown(f"Tip: 按Enter提交, 按Shift+Enter换行。当前模型: {LLM_MODEL} \n {proxy_info}")
+                    with gr.Accordion("基础功能区", open=True) as area_basic_fn:
+                        with gr.Row():
+                            for k in functional:
+                                variant = functional[k]["Color"] if "Color" in functional[k] else "secondary"
+                                functional[k]["Button"] = gr.Button(k, variant=variant)
+                    with gr.Accordion("函数插件区", open=False) as area_crazy_fn:
+                        with gr.Row():
+                            gr.Markdown("注意：以下“红颜色”标识的函数插件需从输入区读取路径作为参数.")
+                        with gr.Row():
+                            for k in crazy_fns:
+                                if not crazy_fns[k].get("AsButton", True): continue
+                                variant = crazy_fns[k]["Color"] if "Color" in crazy_fns[k] else "secondary"
+                                crazy_fns[k]["Button"] = gr.Button(k, variant=variant)
+                                crazy_fns[k]["Button"].style(size="sm")
+                        with gr.Row():
+                            with gr.Accordion("更多函数插件", open=True):
+                                dropdown_fn_list = [k for k in crazy_fns.keys() if not crazy_fns[k].get("AsButton", True)]
+                                with gr.Row():
+                                    dropdown = gr.Dropdown(dropdown_fn_list, value=r"打开插件列表", label="").style(container=False)
+                                with gr.Row():
+                                    plugin_advanced_arg = gr.Textbox(show_label=True, label="高级参数输入区", visible=False, 
+                                                                    placeholder="这里是特殊函数插件的高级参数输入区").style(container=False)
+                                with gr.Row():
+                                    switchy_bt = gr.Button(r"请先从插件列表中选择", variant="secondary")
+                        with gr.Row():
+                            with gr.Accordion("点击展开“文件上传区”。上传本地文件可供红色函数插件调用。", open=False) as area_file_up:
+                                file_upload = gr.Files(label="任何文件, 但推荐上传压缩文件(zip, tar)", file_count="multiple")
+                    with gr.Accordion("更换模型 & SysPrompt & 交互界面布局", open=(LAYOUT == "TOP-DOWN")):
+                        system_prompt = gr.Textbox(show_label=True, placeholder=f"System Prompt", label="System prompt", value=initial_prompt)
+                        top_p = gr.Slider(minimum=-0, maximum=1.0, value=1.0, step=0.01,interactive=True, label="Top-p (nucleus sampling)",)
+                        temperature = gr.Slider(minimum=-0, maximum=2.0, value=1.0, step=0.01, interactive=True, label="Temperature",)
+                        max_length_sl = gr.Slider(minimum=256, maximum=4096, value=512, step=1, interactive=True, label="Local LLM MaxLength",)
+                        checkboxes = gr.CheckboxGroup(["基础功能区", "函数插件区", "底部输入区", "输入清除键", "插件参数区"], value=["基础功能区", "函数插件区"], label="显示/隐藏功能区")
+                        md_dropdown = gr.Dropdown(AVAIL_LLM_MODELS, value=LLM_MODEL, label="更换LLM模型/请求源").style(container=False)
 
-                    gr.Markdown(description)
-                with gr.Accordion("备选输入区", open=True, visible=False) as area_input_secondary:
-                    with gr.Row():
-                        txt2 = gr.Textbox(show_label=False, placeholder="Input question here.", label="输入区2").style(container=False)
-                    with gr.Row():
-                        submitBtn2 = gr.Button("提交", variant="primary")
-                    with gr.Row():
-                        resetBtn2 = gr.Button("重置", variant="secondary"); resetBtn2.style(size="sm")
-                        stopBtn2 = gr.Button("停止", variant="secondary"); stopBtn2.style(size="sm")
-                        clearBtn2 = gr.Button("清除", variant="secondary", visible=False); clearBtn2.style(size="sm")
+                        gr.Markdown(description)
+                    with gr.Accordion("备选输入区", open=True, visible=False) as area_input_secondary:
+                        with gr.Row():
+                            txt2 = gr.Textbox(show_label=False, placeholder="Input question here.", label="输入区2").style(container=False)
+                        with gr.Row():
+                            submitBtn2 = gr.Button("提交", variant="primary")
+                        with gr.Row():
+                            resetBtn2 = gr.Button("重置", variant="secondary"); resetBtn2.style(size="sm")
+                            stopBtn2 = gr.Button("停止", variant="secondary"); stopBtn2.style(size="sm")
+                            clearBtn2 = gr.Button("清除", variant="secondary", visible=False); clearBtn2.style(size="sm")
+        with gr.Tab("新功能试验区"):   
+            with gr.Row():
+                image_input_a = gr.Image()
+                image_output_a = gr.Image()
+            image_button = gr.Button("翻转图片")
+
+            with gr.Row():
+                with gr.Column(scale=1, min_width=600):
+                    gr.Markdown(f"布局测试")
+
+            
+        #新区互动按钮
+        import numpy as np
+        def flip_image(x):
+            return np.fliplr(x)
+        image_button.click(flip_image, inputs=image_input_a, outputs=image_output_a)
+
         # 功能区显示开关与功能区的互动
         def fn_area_visibility(a):
             ret = {}
